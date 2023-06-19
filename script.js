64bb19e3e2d2b7e2057b539996a3563e3c7de8bf
Gabriel Sales <109556645+Neroviih@users.noreply.github.com>
2023-06-19 14:20:50 -0300

    Search bar funcional

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+// obtem a referencia dos elementos HTML
+const searchInput = document.getElementById('searchInput');
+const gallery = document.getElementById('gallery');
 
+//Obtém os itens originais da lista
+const originalItems = Array.from(gallery.getElementsByClassName('content'));
+
+//Adiciona um evento de input á barra de pesquisa
+searchInput.addEventListener('input', function(){
+    const searchTerm = searchInput.value.toLowerCase();;
+
+    //Filtra os itens com base no termo de pesquisa
+    const filteredItems = originalItems.filter(function(item){
+        const itemName = item.getElementsByTagName('h3')[0].textContent.toLowerCase();
+        return itemName.includes(searchTerm);
+    });
+
+    //Remove todos os itens da galeria 
+    while(gallery.firstChild) {
+        gallery.removeChild(gallery.firstChild);
+    }
+
+    //Adiciona os itens filtrados à galeria
+    filteredItems.forEach(function(item){
+        gallery.appendChild(item);
+    });
+
+});
 
