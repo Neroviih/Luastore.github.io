37e4f1ef799a846a3830f8a4d5f7f11aef8f91be
Gabriel Sales <109556645+Neroviih@users.noreply.github.com>
2023-06-19 12:16:41 -0300

    Changes

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ window.open
 ('https://google.com',
 '_blank'
 );
+
+const abaixarHeader = document.getElementById("index_header__ativo");
+
+abaixarHeader.addEventListener('click',function (){
+    if(window.innerWidth < 1000){
+        abaixarHeader.classList.toggle("active");
+    }
+})
+
+
